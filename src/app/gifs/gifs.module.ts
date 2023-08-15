8c431f60f9dc0a555f429a39ff7e7cde317aff4d
DylanVallejo <dylan1811vallejo@gmail.com>
2023-08-15 00:13:01 -0500

    creando u mcomponnete de lista

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { SearchBoxComponent } from './components/search-box/search-box.component';
+import { CardsListComponent } from './components/cards-list/cards-list.component';
 
 
 
@@ -9,7 +10,8 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
 @NgModule({
   declarations: [
     HomePageComponent,
-    SearchBoxComponent
+    SearchBoxComponent,
+    CardsListComponent
   ],
   imports: [
     CommonModule
