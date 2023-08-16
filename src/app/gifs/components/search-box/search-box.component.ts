4c01eb540c8f5729360deeb1c3126125d407bbec
DylanVallejo <dylan1811vallejo@gmail.com>
2023-08-15 23:35:38 -0500

    usando viewChild para tener una referencia local de un input y obtener su valor

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,19 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
     // templateUrl:
   template:`
   <h5>Buscar:</h5>
+    <!-- se utiliza referencia local par ano acceder al forms module -->
+
   <input
     type="text"
     class="form-control"
     placeholder="Buscar gifs.."
+    (keyup.enter)="searchTag( )"
+    #txtTagInput
   />
   `
 })
 export class SearchBoxComponent {
+
+  @ViewChild( 'txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+
   constructor(){
 
+  }
+  // searchTag(newTag:string){
+  searchTag( ){
+    const newTag = this.tagInput.nativeElement.value;
+
+    console.log({newTag});
   }
 }
